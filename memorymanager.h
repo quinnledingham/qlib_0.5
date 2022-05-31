e8708949622b2747de05d17c4ff5d72a5ce123e6
Quinn <44106439+quinnledingham@users.noreply.github.com>
2022-05-30 18:41:07 -0600

    Update memorymanager.h

--- memorymanager.h
+++ memorymanager.h
@@ -1,9 +1,18 @@
 #ifndef MEMORYMANAGER_H
 #define MEMORYMANAGER_H
 
+struct FreeMemory
+{
+    void *Data;
+    int Size;
+};
+
 struct MemoryManager
 {
     char* Next;
+    
+    FreeMemory Free[100];
+    int FreeIndex = 0;
 };
 
 global_variable MemoryManager Manager;
@@ -25,18 +34,50 @@ MemoryCopy(void* Dest, void* Source, int Size)
 internal void*
 qalloc(void* newM, int size)
 {
+    MemoryCopy(Manager.Next, &size, sizeof(int));
+    Manager.Next += sizeof(int);
+    
     MemoryCopy(Manager.Next, newM, size);
     void* r = (void*)Manager.Next;
     Manager.Next += size;
+    
     return r;
 }
 
 internal void*
 qalloc(int size)
 {
+    MemoryCopy(Manager.Next, &size, sizeof(int));
+    Manager.Next += sizeof(int);
+    
     void* r = Manager.Next;
     Manager.Next += size;
+    
     return r;
+}
+
+internal void
+dalloc(void* Storage)
+{
+    void* i = (char*)Storage - sizeof(int);
+    int Size = *(int*)i;
+    
+    //Manager.Next -= Size;
+    memset(i, 0, sizeof(int));
+    memset(Storage, 0, Size);
+    
+    /*
+    Manager.Free[Manager.FreeIndex].Data = i;
+    Manager.Free[Manager.FreeIndex].Size = Size;
+    Manager.FreeIndex++;
+    
+    if (Manager.FreeIndex > 99) {
+        for (int i = 0; i < Manager.FreeIndex; i++) {
+            void* NextMemory = Manager.Free[i].Data + Manager.Free[i].Size;
+            
+        }
+    }
+*/
 }
 
 
