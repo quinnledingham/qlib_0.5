cb71ddb4e306e433ee8693eb82919f7624095a6e
Quinn <44106439+quinnledingham@users.noreply.github.com>
2022-07-18 18:43:28 -0500

    SDL

---
+++ sdl_application.cpp
@@ -0,0 +1,142 @@
+global_variable bool32 GlobalRunning;
+
+internal void
+SDLProcessPendingEvents()
+{
+    SDL_Event Event;
+    
+    while (SDL_PollEvent(&Event))
+    {
+        switch (Event.type)
+        {
+            case SDL_QUIT:
+            {
+                GlobalRunning = false;
+            } break;
+            
+            case SDL_WINDOWEVENT:
+            {
+                switch (Event.window.event)
+                {
+                    case SDL_WINDOWEVENT_RESIZED:
+                    {
+                        //update_screen_size(Event.window.data1, Event.window.data2);
+                        
+                    } break;
+                    
+                    case SDL_WINDOWEVENT_SIZE_CHANGED:
+                    {
+                        printf("SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)\n", Event.window.data1, Event.window.data2);
+                    } break;
+                }
+                
+            } break;
+            
+            default: break;
+        }
+    }
+}
+
+struct sdl
+{
+    SDL_Window *Window;
+    SDL_Renderer *Renderer;
+    SDL_GLContext Context;
+};
+
+internal bool
+MainLoop(sdl *SDL, platform *p)
+{
+    SDLProcessPendingEvents();
+    
+    SDL_GetWindowSize(SDL->Window, &p->Dimension.Width, &p->Dimension.Height);
+    
+    //UpdateRender(p);
+    
+    printf("yo\n");
+    
+    //SDL_GL_SwapWindow(SDL->Window);
+    
+    if (GlobalRunning) return true;
+    else return false;
+}
+
+int main(int argc, char* args[])
+{
+    //PlatformSetCD(CurrentDirectory);
+    
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC | SDL_INIT_AUDIO);
+    
+    // Scuffed way of setting screen size
+    SDL_DisplayMode DM;
+    SDL_GetCurrentDisplayMode(0, &DM);
+    int SDLScreenWidth = DM.w;
+    int SDLScreenHeight = DM.h;
+    int SDLClientWidth = (int)ClientWidth;
+    int SDLClientHeight = (int)ClientHeight;
+    
+    if (ClientWidth < 1) SDLClientWidth = (int)(ClientWidth * SDLScreenWidth);
+    if (ClientHeight < 1) SDLClientHeight = (int)(ClientHeight * SDLScreenHeight);
+    
+    if (ClientWidth == 0) SDLClientWidth = SDLClientHeight;
+    if (ClientHeight == 0) SDLClientHeight = SDLClientWidth;
+    
+    sdl SDL = {};
+    
+#if QLIB_OPENGL
+    SDL_GL_LoadLibrary(NULL);
+    
+    // Request an OpenGL 4.5 context (should be core)
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    
+    // Also request a depth buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    
+    SDL.Window = SDL_CreateWindow(WindowName, 
+                                  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+                                  SDLClientWidth, SDLClientHeight, 
+                                  SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    
+    SDL.Context = SDL_GL_CreateContext(SDL.Window);
+    
+    // Check OpenGL properties
+    printf("OpenGL loaded\n");
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version:  %s\n", glGetString(GL_VERSION));
+    
+    glGenVertexArrays(1, &gVertexArrayObject);
+    glBindVertexArray(gVertexArrayObject);
+    
+    SDL_GL_SetSwapInterval(1);
+    
+#else // QLIB_OPENGL
+    SDL.Renderer = SDL_CreateRenderer(SDL.Window, -1, SDL_RENDERER_PRESENTVSYNC);
+#endif // QLIB_OPENGL
+    
+    platform p = {};
+    
+    p.Memory.PermanentStorageSize = Permanent_Storage_Size;
+    p.Memory.TransientStorageSize = Transient_Storage_Size;
+    
+    // TODO(casey): Handle various memory footprints (USING SYSTEM METRICS)
+    uint64 TotalSize = p.Memory.PermanentStorageSize + p.Memory.TransientStorageSize;
+    
+    p.Memory.PermanentStorage = SDL_malloc(TotalSize);
+    p.Memory.TransientStorage = ((uint8 *)p.Memory.PermanentStorage + p.Memory.PermanentStorageSize);
+    
+    GlobalRunning = true;
+    
+    while (MainLoop(&SDL, &p));
+    
+    SDL_DestroyRenderer(SDL.Renderer);
+    SDL_DestroyWindow(SDL.Window);
+    
+    SDL_Quit();
+    
+    return 0;
+}
